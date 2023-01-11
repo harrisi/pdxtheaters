014a5ca5f41204dc6987ade882dd8ea55500b13c
Ian Harris <ian@fofgof.xyz>
2023-01-10 16:56:41 -0800

    try out mongodb atlas

--- src/routes/api/cron/+server.ts
+++ src/routes/api/cron/+server.ts
@@ -1,6 +1,63 @@
 import { json, error } from '@sveltejs/kit'
 import type { RequestHandler } from './$types'
-import { API_SECRET_KEY } from '$env/static/private'
+import { API_SECRET_KEY, MONGODB_URI } from '$env/static/private'
+import { MongoClient, ServerApiVersion } from 'mongodb'
+
+const client = new MongoClient(MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+})
+
+async function run() {
+  try {
+    await client.connect()
+
+    // should be narrowed?
+    const database = client.db('previewDB')
+
+    // XXXX-XX-XX
+    const today = new Date().toLocaleString('en-US', { month: '2-digit', day: '2-digit', year: '2-digit', timeZone: 'America/Los_Angeles' })
+
+    interface Showing {
+      movie: string
+      theater: string
+      showtime: Date
+    }
+
+    const collection = database.collection<Showing>(today)
+
+    const timeOpts = {
+      hour: 'numeric',
+      minute: 'numeric',
+    }
+
+    // scrape sites to populate showtimes
+    // for (let theater in theaters) {
+
+    // }
+
+    let doc: Showing = {
+      theater: 'theater name',
+      movie: 'movie title',
+      showtime: new Date(),
+    }
+
+    let doc2: Showing = {
+      theater: 'other theater name',
+      movie: 'other movie title',
+      showtime: new Date(),
+    }
+
+    await collection.insertMany([doc, doc2])
+
+    // const docCount = await collection.countDocuments({})
+  } finally {
+    await client.close()
+  }
+}
+
+run().catch(console.error)
 
 export const POST = (async ({ request }) => {
   const foo = await request.json()
